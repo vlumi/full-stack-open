4896f3ac51ba42e54945a76befbf61bf60365c3f
Ville Misaki <ville@misaki.fi>
2020-06-30 17:53:36 +0900

    Add implementation of unicafe.

--- osa1/unicafe/src/index.js
+++ osa1/unicafe/src/index.js
@@ -1,17 +1,78 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const Button = ({ handleClick, label }) => (
+  <button onClick={handleClick}>{label}</button>
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const StatisticLine = ({ text, value }) => (
+  <tr>
+    <th>{text}:</th>
+    <td>{value}</td>
+  </tr>
+);
+const Average = ({ good, bad, total }) => {
+  const score = good - bad;
+  const average = total === 0 ? 0 : score / total;
+  return (
+    <tr>
+      <th>Average:</th>
+      <td>{average}</td>
+    </tr>
+  );
+};
+const Positive = ({ good, total }) => {
+  const positive = total === 0 ? 0 : (good * 100) / total;
+  return (
+    <tr>
+      <th>Positive:</th>
+      <td>{positive} %</td>
+    </tr>
+  );
+};
+const Statistics = ({ good, neutral, bad }) => {
+  const total = good + neutral + bad;
+  if (total === 0) {
+    return (
+      <>
+        <h1>Statistics</h1>
+        <div>"No feedback given"</div>
+      </>
+    );
+  }
+  return (
+    <>
+      <h1>Statistics</h1>
+      <table>
+        <tbody>
+          <StatisticLine text="Good" value={good} />
+          <StatisticLine text="Neutral" value={neutral} />
+          <StatisticLine text="Bad" value={bad} />
+          <Average good={good} bad={bad} total={total} />
+          <Positive good={good} total={total} />
+        </tbody>
+      </table>
+    </>
+  );
+};
+
+const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  return (
+    <>
+      <h1>Give feedback</h1>
+      <p>
+        <Button handleClick={() => setGood(good + 1)} label="Good" />
+        <Button handleClick={() => setNeutral(neutral + 1)} label="Neutral" />
+        <Button handleClick={() => setBad(bad + 1)} label="Bad" />
+      </p>
+      <Statistics good={good} neutral={neutral} bad={bad} />
+    </>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
