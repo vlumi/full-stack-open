4186ca2905bc047610d551837a4ea1fd2ce808d5
Ville Misaki <ville@misaki.fi>
2020-07-01 15:03:00 +0900

    Add puhelinluettelo osa 2c.

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -1,13 +1,19 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import Filter from "./components/Filter";
 import AddPerson from "./components/AddPerson";
 import Persons from "./components/Persons";
 
-const App = (props) => {
-  const [persons, setPersons] = useState(props.initial);
+const App = () => {
+  const [persons, setPersons] = useState([]);
   const [filterName, setFilterName] = useState("");
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
+
+  const DB_URL = "http://localhost:3001/persons";
+  useEffect(() => {
+    axios.get(DB_URL).then((response) => setPersons(response.data));
+  }, []);
 
   const filteredPersons =
     filterName.length === 0
