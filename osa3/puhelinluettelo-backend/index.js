322cb5535c65fc018fc1b9ccd52632e1615c67e1
Ville Misaki <ville@misaki.fi>
2020-07-07 13:31:37 +0900

    Implement puhelinluettelo up to 3.18, including MongoDB backend.

--- osa3/puhelinluettelo-backend/index.js
+++ osa3/puhelinluettelo-backend/index.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
@@ -14,57 +15,41 @@ app.use(
   morgan(":method :url :status :res[content-length] - :response-time ms :body")
 );
 
-let persons = [
-  {
-    name: "Arto Hellas",
-    number: "040-123456",
-    id: 1,
-  },
-  {
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-    id: 2,
-  },
-  {
-    name: "Dan Abramov",
-    number: "12-43-234345",
-    id: 3,
-  },
-  {
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-    id: 4,
-  },
-];
+const Person = require("./models/persons");
 
-app.get("/info", (req, res) => {
-  const content =
-    `<p>Phonebook has info for ${persons.length} people</p>` +
-    `<p>${new Date()}</p>`;
-  res.send(content);
+app.get("/info", (req, res, next) => {
+  Person.find({})
+    .then((persons) => {
+      const content =
+        `<p>Phonebook has info for ${persons.length} people</p>` +
+        `<p>${new Date()}</p>`;
+      res.send(content);
+    })
+    .catch((error) => next(error));
 });
-app.get("/api/persons", (req, res) => {
-  res.json(persons);
-});
-app.get("/api/persons/:id", (req, res) => {
-  const id = Number(req.params.id);
-  const person = persons.find((person) => person.id === id);
-  if (person) {
-    res.json(person);
-  } else {
-    res.status(404).end();
-  }
+app.get("/api/persons", (req, res, next) =>
+  Person.find({})
+    .then((persons) => res.json(persons))
+    .catch((error) => next(error))
+);
+app.get("/api/persons/:id", (req, res, next) => {
+  Person.findById(req.params.id)
+    .then((person) => {
+      if (person) {
+        res.json(person);
+      } else {
+        res.status(404).end();
+      }
+    })
+    .catch((error) => next(error));
 });
 const generateId = () => Math.floor(Math.random() * 1000000000);
-app.post("/api/persons", (req, res) => {
-  const body = req.body;
+app.post("/api/persons", (req, res, next) => {
   var errors = [];
-  if (!body.name) {
+  if (!req.body.name) {
     errors.push("Name must be present");
-  } else if (persons.findIndex((person) => person.name === body.name) >= 0) {
-    errors.push("Name must be unique");
   }
-  if (!body.number) {
+  if (!req.body.number) {
     errors.push("Number must be present");
   }
   if (errors.length > 0) {
@@ -74,26 +59,48 @@ app.post("/api/persons", (req, res) => {
     });
   }
 
-  const person = {
-    name: body.name,
-    number: body.number,
+  const person = new Person({
+    name: req.body.name,
+    number: req.body.number,
     id: generateId(),
+  });
+
+  person
+    .save()
+    .then((savedPerson) => {
+      res.json(savedPerson);
+    })
+    .catch((error) => next(error));
+});
+app.put("/api/persons/:id", (req, res, next) => {
+  const person = {
+    number: req.body.number,
   };
-
-  persons = persons.concat(person);
-
-  res.json(person);
+  Person.findByIdAndUpdate(req.params.id, person, { new: true })
+    .then((updatedPerson) => res.json(updatedPerson))
+    .catch((error) => next(error));
 });
-app.delete("/api/persons/:id", (req, res) => {
-  const id = Number(req.params.id);
-  persons = persons.filter((person) => person.id !== id);
-
-  res.status(204).end();
+app.delete("/api/persons/:id", (req, res, next) => {
+  Person.findByIdAndDelete(req.params.id)
+    .then((result) => res.status(204).end())
+    .catch((error) => next(error));
 });
-const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: "unknown endpoint" });
+
+const unknownEndpoint = (req, res, next) => {
+  res.status(404).send({ error: "unknown endpoint" });
 };
 app.use(unknownEndpoint);
+
+const errorHandler = (error, req, res, next) => {
+  console.error(error.message);
+
+  if (error.name === "CastError") {
+    return res.status(400).send({ error: "malformatted id" });
+  }
+
+  next(error);
+};
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
